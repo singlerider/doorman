b11b3f5997823177bf0a7b856c5001aea63236dd
Shane <haroboy876@gmail.com>
2015-08-16 07:21:36 -0700

    hello init

--- run.py
+++ run.py
@@ -1,24 +1,15 @@
-from flask import Flask, render_template
-from twilio.util import TwilioCapability
+from flask import Flask
+import twilio.twiml
  
 app = Flask(__name__)
  
-@app.route('/client', methods=['GET', 'POST'])
-def client():
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
     """Respond to incoming requests."""
+    resp = twilio.twiml.Response()
+    resp.say("Hello Monkey")
  
-    account_sid = "ACf7ddb8dcf3bae9690df1b481ee5dcf9a"
-    auth_token = "17a14e7b1d5213d0d12aefd7756d195"
- 
-    # This is a special Quickstart application sid - or configure your own
-    # at twilio.com/user/account/apps
-    application_sid = "APabe7650f654fc34655fc81ae71caa3ff"
- 
-    capability = TwilioCapability(account_sid, auth_token)
-    capability.allow_client_outgoing(application_sid)
-    token = capability.generate()
- 
-    return render_template('client.html', token=token)
+    return str(resp)
  
 if __name__ == "__main__":
     app.run(debug=True)
