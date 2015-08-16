be3db6933e8b011c27567bdf70007a3c293f1fae
Shane <haroboy876@gmail.com>
2015-08-16 08:09:59 -0700

    asldklkasl

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 import twilio.twiml
  
@@ -12,4 +13,5 @@ def hello_monkey():
     return str(resp)
  
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
